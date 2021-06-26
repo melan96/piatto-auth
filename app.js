af38c9f0026af27bb4fe892b8b94bf4b5c724b6e
Melan Rashitha Dias <it17006712@my.sliit.lk>
2021-06-26 23:41:47 +0530

    auth routes added

--- app.js
+++ app.js
@@ -2,24 +2,21 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 require("dotenv").config();
-const http = require("http");
-
+const https = require("https");
+const { json } = require("express");
+const request = require("request");
+const jwt = require("jsonwebtoken");
 const app = express();
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-const Rpit = express.Router();
-Rpit.get("/", (req, res) => {
-  http.get(
-    "https://batterybroadcaster.herokuapp.com/batteryinfo/getlatest/60adc87c1ccc1e055979ff25",
-    (resp, incM) => {
-      res.json(resp).sendStatus(200);
-      console.log(incM);
-    }
-  );
+app.post("/auth", (req, res) => {
+  console.log(req.body);
+
+  //TODO : Auth implementations should be there
 });
-app.use("/", Rpit);
+
 app.listen(process.env.PORT, () => {
   console.log("Application Listen on PORT " + process.env.PORT);
 });
